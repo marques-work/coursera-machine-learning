94a33f46efb7b0c5110df3a8b78e773ef25a59a4
Marques Lee <marques.lee@thoughtworks.com>
2017-11-08 14:17:44 -0800

    implement normal equation
    change iterations back to 400 as it gives the more accurate price, similar to normal equation

--- ex1_multi.m
+++ ex1_multi.m
@@ -83,7 +83,7 @@ fprintf('Running gradient descent ...\n');
 
 % Choose some alpha value
 alpha = 0.1;
-num_iters = 50;
+num_iters = 400;
 
 % Init Theta and Run Gradient Descent
 theta = zeros(3, 1);
@@ -149,7 +149,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = [1 1650 3] * theta;
 
 
 % ============================================================
