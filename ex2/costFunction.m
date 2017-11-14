700701c8bc3d981e05a89df77f1241852a00a3da
Marques Lee <marques.lee@thoughtworks.com>
2017-11-14 10:51:18 -0800

    Implement cost function for logistical regression

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -4,29 +4,12 @@ function [J, grad] = costFunction(theta, X, y)
 %   parameter for logistic regression and the gradient of the cost
 %   w.r.t. to the parameters.
 
-% Initialize some useful values
-m = length(y); % number of training examples
+  % Initialize some useful values
+  m = length(y); % number of training examples
 
-% You need to return the following variables correctly
-J = 0;
-grad = zeros(size(theta));
+  ytx = y';
+  hx = sigmoid(X * theta);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-%
-% Note: grad should have the same dimensions as theta
-%
-
-
-
-
-
-
-
-
-% =============================================================
-
+  J = (-ytx * log(hx) - ((1 - ytx) * log(1 - hx))) / m;
+  grad = (X' * (hx - y)) / m;
 end
